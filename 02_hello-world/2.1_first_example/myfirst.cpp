79c13742f76819b6565790aade2447bf51e33bf5
walkacross <yujiangallen@126.com>
2018-06-03 17:29:04 +0800

    first example

---
+++ 02_hello-world/2.1_first_example/myfirst.cpp
@@ -0,0 +1,11 @@
+// myfirst.cpp -- display a message
+
+#include <iostream>                            // a PREPROCESSOR directive
+int main()                                     // function header
+{
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;                             // start a new line
+    cout << "you won't regret it" << endl;
+    return 0;                                  // terminate main()
+}
---
+++ 02_hello-world/2.1_first_example/readme.md
@@ -0,0 +1,13 @@
+# hello-world example
+
+## step1: write source code
+
+## step2: compile it
+~~~
+g++ myfirst.cpp
+~~~
+
+## step3: run it
+~~~
+./a.out
+~~~
